3fa4ad548a08cf4935f8e56d063137c23b038e73
Leonardo Beltran <leonardo.casallas.beltran@gmail.com>
2020-04-09 11:39:50 -0500

    se enlazan todos los compenentes en el home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,31 +1,35 @@
-import React, {useContext} from 'react'
-import Header from '../components/Header'
-import ListOfCards from '../components/ListOfCards'
-import InformationContext from '../Context/Information'
-//import Footer from '../components/Footer'
-//import Ads from '../components/Ads'
+import React, { useContext } from 'react';
+import Header from '../components/Header';
+import ListOfCards from '../components/ListOfCards';
+import InformationContext from '../Context/Information';
+import Footer from '../components/Footer';
+import Ads from '../components/Ads';
+import ProductList from '../components/ProductsList';
 
 const Home = () => {
-  const information = useContext(InformationContext)
+  const information = useContext(InformationContext);
 
   return (
     <>
       <Header />
+      <Ads />
       <section>
-        {information.sections.map((typeOfProduct)=>{
-            return(
-              <ListOfCards key={typeOfProduct.color}
-                color={typeOfProduct.color}
-                title={typeOfProduct.title}
-                id={typeOfProduct.id}
-                subtitle={[typeOfProduct.subTitle]}
-              />
-            )
-          })
-        }
+        {information.sections.map((typeOfProduct) => {
+          return (
+            <ListOfCards
+              key={typeOfProduct.color}
+              color={typeOfProduct.color}
+              title={typeOfProduct.title}
+              id={typeOfProduct.id}
+              subtitle={[typeOfProduct.subTitle]}
+            />
+          );
+        })}
       </section>
+      <ProductList />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Home;
