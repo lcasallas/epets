2239d46ce75d610f74e4d1e99d31b2c6011312e5
Leonardo Beltran <leonardo.casallas.beltran@gmail.com>
2020-04-09 17:59:59 -0500

    correccion header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import '../assets/styles/Header.css'
-import Logo from '../assets/statics/epetslogo.png'
+import React from 'react';
+import '../assets/styles/Header.css';
+import Logo from '../assets/static/epetslogo.png';
 
 const Header = () => (
-    <header>
-        <img className="logo" src={Logo} />
-        <nav>
-            <ul className="nav-links">
-            <a href='/'>
-                <li>Juguetes</li>
-            </a>
-            <a href='/'>
-                <li>Comida</li>
-            </a>
-            <a href='/'>
-                <li>Ropa</li>
-            </a>
-            <a href='/'>
-                <li>Estetica</li>
-            </a>
-            <a href='/'>
-                <li>Consulta Medica</li>
-            </a>
-        </ul>
-        </nav>
-        
-    </header>
-)
+  <header>
+    <img className='logo' src={Logo} />
+    <nav>
+      <ul className='nav-links'>
+        <a href='/'>
+          <li>Juguetes</li>
+        </a>
+        <a href='/'>
+          <li>Comida</li>
+        </a>
+        <a href='/'>
+          <li>Ropa</li>
+        </a>
+        <a href='/'>
+          <li>Estetica</li>
+        </a>
+        <a href='/'>
+          <li>Consulta Medica</li>
+        </a>
+      </ul>
+    </nav>
+  </header>
+);
 
-export default Header
+export default Header;
