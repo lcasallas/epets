ff50051b96c957ab50b33784d2c4405a7e038227
Leonardo Beltran <leonardo.casallas.beltran@gmail.com>
2020-04-09 17:47:17 -0500

    correccion home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import Home from './pages/Home';
-import ColorContext, {Information} from './Context/Information';
+import React from 'react';
+import Home from './pages/Home.js';
+import ColorContext, { Information } from './Context/Information';
 
 const App = () => {
   return (
     <ColorContext.Provider value={Information.data}>
       <Home />
     </ColorContext.Provider>
-  )
-}
-
+  );
+};
 
 export default App;
