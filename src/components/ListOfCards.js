adbba0c36f669e733d6290a7d70ad0278c0bcdc4
César Galindo <csgalindos@hotmail.com>
2020-04-20 18:27:13 -0500

    rev console log

--- src/components/ListOfCards.js
+++ src/components/ListOfCards.js
@@ -1,28 +1,25 @@
-import React from 'react'
-import Card from './Card'
-import '../assets/styles/components/ListOfCards.css'
+import React from "react"
+import Card from "./Card"
+import "../assets/styles/components/ListOfCards.css"
 
-export default function ListOfCards({color, title, subtitle, id }) {
-return (
-  <section>
-    <div className={`cards__title ${id}`}>
-      <h2>{title}</h2>
-    </div>
-    <div className='cards'>
-      {subtitle.map(item=>{
-        console.log(subtitle)
-        return(
-            item.map((dataCard)=>{
-              return (
-                <div key={dataCard.name} className='cards__container'>
-                  <Card color={color} data={dataCard} />
-                </div>
-              )
-            })
-        )
-      })
-      }
-    </div>
-  </section>
-)
+export default function ListOfCards({ color, title, subtitle, id }) {
+  return (
+    <section>
+      <div className={`cards__title ${id}`}>
+        <h2>{title}</h2>
+      </div>
+      <div className='cards'>
+        {subtitle.map((item) => {
+          console.log(subtitle)
+          return item.map((dataCard) => {
+            return (
+              <div key={dataCard.name} className='cards__container'>
+                <Card color={color} data={dataCard} />
+              </div>
+            )
+          })
+        })}
+      </div>
+    </section>
+  )
 }
