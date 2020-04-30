fd50414c04fb04bd76a7801caf63aff7156251aa
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-30 03:18:43 -0500

    Add SectionProducts component

---
+++ src/components/SectionProducts.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../assets/styles/components/SectionProducts.css';
+import useProducts from '../hooks/useProducts';
+import Card from './Card';
+
+const SectionProducts = ({ section, pet }) => {
+  const products = useProducts(`http://localhost:3001/api/${section}/${pet}`);
+
+  return (
+    <section className='SectionProducts'>
+      {products.map((product) => {
+        const data = {
+          photo: product.image,
+          name: product.name,
+          body: product.description,
+          link: product.url,
+          linkShow: 'Comprar producto'
+        };
+
+        return <Card key={data.name} data={data} color={section} />;
+      })}
+    </section>
+  );
+};
+
+export default SectionProducts;
