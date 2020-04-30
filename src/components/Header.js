ef47439e36326ed06512a1d90689375210b2865e
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-30 03:15:06 -0500

    Add links in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,6 @@ import Modal from './Modal';
 import Calendly from './Calendly';
 import '../assets/styles/Header.css';
 import Logo from '../assets/static/epetslogo.png';
-import dogIcon from '../assets/static/dogIcon.png';
-import catIcon from '../assets/static/catIcon.png';
-import fishIcon from '../assets/static/fishIcon.png';
-import birdIcon from '../assets/static/birdIcon.png';
-import reptileIcon from '../assets/static/reptileIcon.png';
-import mamalIcon from '../assets/static/mamalIcon.png';
 import cookieIcon from '../assets/static/cookieIcon.png';
 import petToy from '../assets/static/pettoy.png';
 import dogFood from '../assets/static/dogfood.png';
@@ -16,42 +10,50 @@ import clothes from '../assets/static/tshirt.png';
 import stetic from '../assets/static/stetic.png';
 import medic from '../assets/static/consulta.png';
 import bordContact from '../assets/static/birdcontact.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState({open: false, calendlyType: ''});
 
   return (
     <>
       <header className='Header'>
-        <img className='logo' src={Logo} />
+        <Link to='/'>
+          <img className='logo' src={Logo} />
+        </Link>
         <nav>
           <ul className='nav-links'>
-            <a href='/'>
-              <img src={petToy} className='header-icon'></img>
-              <li>Juguetes</li>
-            </a>
-            <a href='/'>
-              <img src={dogFood} className='header-icon'></img>
-              <li>Comida</li>
-            </a>
-            <a href='/'>
-              <img src={clothes} className='header-icon'></img>
-              <li>Ropa</li>
-            </a>
-            <a href='/'>
-              <img src={stetic} className='header-icon'></img>
-              <li>Estetica</li>
-            </a>
-            <span onClick={()=>{setModal(true)}}>
-              <img src={medic} className='header-icon'></img>
-              <li>Consulta Medica</li>
-            </span>
-
-            <img src={bordContact} className='header-icon'></img>
             <li>
+              <Link to='/section/toys/dogs'>
+                <img src={petToy} className='header-icon'></img>
+                <span>Juguetes</span>
+              </Link>
+            </li>
+            <li>
+              <Link to='/section/food/dogs'>
+                <img src={dogFood} className='header-icon'></img>
+                <span>Comida</span>
+              </Link>
+            </li>
+            <li>
+              <Link to='/section/clothes/dogs'>
+                <img src={clothes} className='header-icon'></img>
+                <span>Ropa</span>
+              </Link>
+            </li>
+            <li onClick={()=>{setModal({open: true, calendlyType: 'estetica'})}}>
+                <img src={stetic} className='header-icon'></img>
+                <span>Estética</span>
+            </li>
+            <li onClick={()=>{setModal({open: true, calendlyType: 'consulta'})}}>
+              <img src={medic} className='header-icon'></img>
+              <span>Consulta Medica</span>
+            </li>
+            <li>
+              <img src={bordContact} className='header-icon'></img>
               <a
                 href='mailto:epets@animals.world.com?Subject=Hola%20Amigos%20de%20Epets!'
-                target='_top'
+                target='_blank'
               >
                 Contactanos
               </a>
@@ -60,8 +62,8 @@ const Header = () => {
         </nav>
         <img src={cookieIcon} className='cookie-menu'></img>
       </header>
-      <Modal isOpen={modal} onClose={()=>{setModal(false)}}>
-        <Calendly />
+      <Modal isOpen={modal.open} onClose={()=>{setModal(false)}}>
+        <Calendly type={modal.calendlyType} />
       </Modal>
     </>
   );
