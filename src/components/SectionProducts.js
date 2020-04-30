85f20ddd89150d69adb199a07fc8ccff9995b939
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-30 12:45:00 -0500

    Update SectionProducts to list all products

--- src/components/SectionProducts.js
+++ src/components/SectionProducts.js
@@ -3,21 +3,49 @@ import '../assets/styles/components/SectionProducts.css';
 import useProducts from '../hooks/useProducts';
 import Card from './Card';
 
-const SectionProducts = ({ section, pet='' }) => {
-  const products = useProducts(`https://epets-pet-market.herokuapp.com/api/${section}/${pet}`);
+const SectionProducts = ({ section, pet = '' }) => {
+  const products = useProducts(
+    `https://epets-pet-market.herokuapp.com/api/${section}/${pet}`
+  );
+
+  if (!pet && Object.values(products).length) {
+    const productsArray = [];
+
+    products.forEach((section) => {
+      Object.values(section).forEach((product) => {
+        productsArray.push(product);
+      });
+    });
+
+    return (
+      <section className="SectionProducts">
+        {productsArray.map((product, index) => {
+          const data = {
+            photo: product.image,
+            name: product.name,
+            body: product.description,
+            link: product.url,
+            linkShow: 'Comprar producto',
+          };
+
+          return <Card key={index} data={data} color={section} />;
+        })}
+      </section>
+    );
+  }
 
   return (
-    <section className='SectionProducts'>
-      {products.map((product) => {
+    <section className="SectionProducts">
+      {products.map((product, index) => {
         const data = {
           photo: product.image,
           name: product.name,
           body: product.description,
           link: product.url,
-          linkShow: 'Comprar producto'
+          linkShow: 'Comprar producto',
         };
 
-        return <Card key={data.name} data={data} color={section} />;
+        return <Card key={index} data={data} color={section} />;
       })}
     </section>
   );
