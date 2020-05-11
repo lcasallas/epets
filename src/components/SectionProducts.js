66577d75cca22fe23f3e231cb971bf4802ecc8a6
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-05-10 23:46:34 -0500

    Add medic section

--- src/components/SectionProducts.js
+++ src/components/SectionProducts.js
@@ -7,7 +7,9 @@ import Error from './Error';
 
 const SectionProducts = ({ section, pet = '' }) => {
   const { products, loading, error } = useProducts(
-    `https://epets-pet-market.herokuapp.com/api/products/${section}/${pet}`
+    `https://epets-pet-market.herokuapp.com/api/${
+      section === 'medic' ? 'services' : 'products'
+    }/${section}/${pet}`
   );
 
   if (loading) {
@@ -15,16 +17,18 @@ const SectionProducts = ({ section, pet = '' }) => {
   }
 
   if (error) {
-    return <Error message='No se pudieron obtener los productos.'/>;
+    return <Error message="No se pudieron obtener los productos." />;
   }
 
-  if (!pet && Object.values(products).length) {
+  if (!pet && section !== 'medic' && Object.values(products).length) {
     const productsArray = [];
 
     products.forEach((section) => {
       Object.values(section).forEach((product) => {
         productsArray.push(product);
       });
+
+      console.log(productsArray);
     });
 
     return (
@@ -47,13 +51,24 @@ const SectionProducts = ({ section, pet = '' }) => {
   return (
     <section className="SectionProducts">
       {products.map((product, index) => {
-        const data = {
-          photo: product.image,
-          name: product.name,
-          body: product.description,
-          link: product.url,
-          linkShow: 'Comprar producto',
-        };
+        let data;
+        if (section === 'medic') {
+          data = {
+            photo: product.image,
+            name: product.title,
+            body: product.description,
+            link: product.url,
+            linkShow: 'Más información',
+          };
+        } else {
+          data = {
+            photo: product.image,
+            name: product.name,
+            body: product.description,
+            link: product.url,
+            linkShow: 'Comprar producto',
+          };
+        }
 
         return <Card key={index} data={data} color={section} />;
       })}
