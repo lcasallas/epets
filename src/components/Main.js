e20991e2c30f914f0478b194ffefe07ef8a92f0d
Leonardo Beltran <leonardo.casallas.beltran@gmail.com>
2020-05-13 21:45:18 -0500

    correccion bug header, agrega texto home

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,9 +9,7 @@ export default function Main() {
     </div>
     <div className='main_text'>
       <p>
-      LOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREM
-      LOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREMREMLOREMLOREMLOREMLOREMLOREMLOREM
-      LOREMLOREMLOREMLOREMLOREMLOREMLOREMLOREM
+      Somos tu mejor opción en tienda de mascotas, no solo por tener los mejores precios y ofertas si no por que recibirás la asesoría especializada de Veterinarios y expertos en mascotas para hacer más placentera tu experiencia de compra. Hemos seleccionado las mejores marcas para e-pet. Ofrecemos constantemente promociones y precios especiales en nuestros productos para perros y gatos con la ventaja de acumular puntos en cada compra que podrás redimir en tus próximos pedidos. No olvides que tambien contamos con el servicio de Peluquería Canina y Felina, venta de medicamentos para animales y un completo espacio para atención veterinaria.
       </p>
     </div>
     </div>
